9e58f770977bccae4fb96d3f382600e3aa2cd73c
Tjones <taouris.jones@gmail.com>
2025-11-07 12:48:06 -0500

    commit

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -1,4 +1,32 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class HomeScreen {
+    private Scanner scanner = new Scanner (System.in);
+    private boolean running = true;
+
+    public void Homescreen(){
+        while(running) {
+
+
+
+
+
+
+            String choice = scanner.nextLine();
+
+        }
+        switch(choice){
+            case "1":
+
+
+            case "0":
+
+
+            default:
+
+
+        }
+    }
 }
--- src/main/java/com/pluralsight/MainApp.java
+++ src/main/java/com/pluralsight/MainApp.java
@@ -1,4 +1,8 @@
 package com.pluralsight;
 
 public class MainApp {
+    public static void main(String[] args) {
+        HomeScreen homeScreen = new HomeScreen();
+        homeScreen.Homescreen();
+    }
 }
