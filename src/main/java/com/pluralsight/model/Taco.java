c74e48b4c930e73c7d4299f0b38431c2c9a61718
Tjones <taouris.jones@gmail.com>
2025-11-11 18:18:07 -0500

    commit

--- src/main/java/com/pluralsight/model/Taco.java
+++ src/main/java/com/pluralsight/model/Taco.java
@@ -1,4 +1,35 @@
 package com.pluralsight.model;
 
-public class Taco {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Taco extends MenuItem{
+    private final String size;
+    private final String shell;
+    private final boolean deepFried;
+
+    private final List<Topping> meats = new ArrayList<>();
+    private final List<Topping> cheeses = new ArrayList<>();
+    private final List<Topping> toppings = new ArrayList<>();
+    private final List<String> sauces = new ArrayList<>();
+
+    public Taco(String name, double price, String size, String shell, boolean deepFried) {
+        super(name, price);
+        this.size = size;
+        this.shell = shell;
+        this.deepFried = deepFried;
+    }
+    public void addMeat(Topping meat) {meats.add(meat); }
+    public void addCheese(Topping cheese){ cheeses.add(cheese); }
+    public void addTopping (Topping topping) { toppings.add(topping) ;}
+    public void addSauce(String sauce) {sauces.add(sauce);}
+
+    public double getBasePrice(){
+        String Size = size == null ? "" : size.trim().toLowerCase();
+        if (size.equals("single")) return 3.50;
+        if (size.equals("3-tacos") || size.equals("3-taco")) return 9.00;
+        if (size.equals("burrito")) return 8.50;
+        return 0.0;
+    }
+
 }
